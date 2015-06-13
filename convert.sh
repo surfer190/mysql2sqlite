e8866b6879fee1a9abdeb4e90314a3c7786c4e91
surfer190 <shutch190@gmail.com>
2015-06-13 09:55:17 +0200

    Fix use of variables and readme

--- convert.sh
+++ convert.sh
@@ -1,19 +1,25 @@
-#!/bin/sh 
+#!/bin/sh
 
-DB_NAME="name"
-DB_USER="user"
-DB_PASS="pass"
+# Convert a Mysql Database dump to a sqlite database
+# Script creates the dump then converts
 
-mysqldump --compact --compatible=ansi --default-character-set=binary -u DB_USER -pDB_PASS DB_NAME | 
-grep -v ' KEY "' | 
-grep -v ' UNIQUE KEY "' | 
-perl -e 'local $/;$_=<>;s/,\n\)/\n\)/gs;print "begin;\n";print;print "commit;\n"' | 
-perl -pe ' 
-if (/^(INSERT.+?)\(/) { 
-$a=$1; 
-s/\\'\''/'\'\''/g; 
-s/\\n/\n/g; 
-s/\),\(/\);\n$a\(/g; 
-} 
-' | 
-sqlite3 output.db
+# Change the below values
+DB_NAME="db_name"
+DB_USER="db_user"
+DB_PASS="db_pass"
+
+mysqldump --compact --compatible=ansi --default-character-set=binary -u $DB_USER -p$DB_PASS $DB_NAME |
+grep -v ' KEY "' |
+grep -v ' UNIQUE KEY "' |
+perl -e 'local $/;$_=<>;s/,\n\)/\n\)/gs;print "begin;\n";print;print "commit;\n"' |
+perl -pe '
+if (/^(INSERT.+?)\(/) {
+$a=$1;
+s/\\'\''/'\'\''/g;
+s/\\n/\n/g;
+s/\),\(/\);\n$a\(/g;
+}
+' |
+sqlite3 output.sqlite
+
+#to change the output name change the above filename
--- readme.md
+++ readme.md
@@ -1,3 +1,36 @@
 # mysql2sqlite
 
-A direct port of the [shell script on mySQL forums](http://forums.mysql.com/read.php?145,68269,92627)
+### Usage
+
+1. Clone the repo
+
+  `git clone git@github.com:surfer190/mysql2sqlite.git`
+
+2. Change db values
+
+  `vim convert.sh`
+
+3. Change to your credentials
+
+  ```
+  DB_NAME="db_name"
+  DB_USER="db_user"
+  DB_PASS="db_pass"
+  ```
+
+4. Run the script
+
+    `bash convert.sh`
+
+5. File `output.sqlite` will be created in the same directory
+
+### Other tools
+
+* [Convert Microsoft SQL database (MSSQL) to MySQL](https://github.com/matriphe/mssql2mysql)
+
+
+### License
+
+[GPL](http://www.gnu.org/licenses/gpl-3.0.en.html)
+
+A direct port of the [shell script for MySQL to SQLite db conversion on mySQL forums](http://forums.mysql.com/read.php?145,68269,92627)
